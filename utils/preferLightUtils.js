a73e5658c7f868c038a649d99c333d58588caa6c
Fabito02 <fabianojuniorlimaba2@gmail.com>
2026-07-23 21:57:15 -0300

    fix(preferLightUtils.js): add missing file

---
+++ utils/preferLightUtils.js
@@ -0,0 +1,30 @@
+/*
+ * lightStyle.js
+ *
+ * This file is part of ChromaLeon GNOME Shell Extension.
+ * https://github.com/Fabito02/ChromaLeon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import St from "gi://St";
+
+import { sessionMode } from "resource:///org/gnome/shell/ui/main.js";
+
+export function updateColorScheme(scheme) {
+  sessionMode.colorScheme = scheme;
+  St.Settings.get().notify("color-scheme");
+}
